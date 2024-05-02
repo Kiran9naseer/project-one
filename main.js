9d655e69c2c61d42ff119cc6ed6e307e7ca32211
Kiran9naseer <164726031+Kiran9naseer@users.noreply.github.com>
2024-05-02 18:43:16 +0500

    my second project

---
+++ main.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let maxMax = 840;
+let obtainMax = 450;
+let persenTage = obtainMax * 100 / maxMax;
+if (persenTage >= 90 && persenTage <= 100) {
+    console.log("your Grade is A++");
+}
+else if (persenTage >= 70 && persenTage <= 89) {
+    console.log("your Grade is A");
+}
+else if (persenTage >= 60 && persenTage <= 69) {
+    console.log("your Grade is B");
+}
+else if (persenTage >= 50 && persenTage <= 59) {
+    console.log("your Grade is C");
+}
+else if (persenTage >= 40 && persenTage <= 49) {
+    console.log("your Grade is D");
+}
+else {
+    console.log("you are fail");
+}
+console.log(persenTage);
